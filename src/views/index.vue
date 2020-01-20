cb20f707a5ade485eda3249a886e58cf389c1914
harvestalot <fangjingyu0502@163.com>
2020-01-20 15:08:08 +0800

    优化问题

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,7 +8,7 @@
                 <side-nav></side-nav>
             </a-col>
             <a-col :span="20" class="h_100">
-                <a-row class="h_8 bgc_main header">
+                <a-row class="h_8 bgc_main po_r header">
                     <a-col :span="1">
                         <div class="pointer logo_icon">
                             <img src="../../static/img/logo_icon.png" @click="go_home">
@@ -20,13 +20,14 @@
                     <a-col :span="6" class="h_100">
                         <nav id="nav" class="nav">
                             <router-link :to="{ name: 'Home' }" class="nav_item">首页</router-link>
-                            <span class="nav_item">平台简介</span>
+                            <span class="nav_item" @click="introduction_visible = !introduction_visible">平台简介</span>
                         </nav>
                     </a-col>
+                    <!-- <div class="introduction"></div> -->
                 </a-row>
                 <a-row class="h_92 po_r">
                     <main-map ref="mainMapChild">
-                        <template v-slot="params">
+                        <template>
                             <router-view v-if="isRouterAlive"></router-view>
                         </template>
                     </main-map>
@@ -38,6 +39,15 @@
             <img src="../../static/img/logo-bottom.png" alt="">
             <router-link :to="{ name: 'AboutUs' }" class="about_us">关于我们</router-link>
         </a-row>
+        <a-modal v-model="introduction_visible" centered :footer="null" width="70%">
+            <div class="introduction_content">
+                <h1 class="introduction_title">简介</h1>
+                <p>东城区，隶属于北京市，地处北京市中心城区的东部，东、北与朝阳区接壤，南与丰台区相连，西与西城区毗邻，介于东经116°22'17"—116°26'46"，北纬39°51'26"—39°58'22"之间，东西最大距离5.2千米，南北最大距离13千米，总面积41.84平方千米。</p>
+                <p>东城区是北京文物古迹最为集中的区域，拥有国家级文物保护单位16处，市级文物保护单位60处，北京市历史文化保护区18.5片。</p>
+                <p>2018年，东城区下辖17个街道，常住人口82.2万人，实现地区生产总值（GDP）2425.7亿元，其中，第二产业实现增加值89.6亿元，第三产业实现增加值2336.1亿元，按常住人口计算，全区人均地区生产总值达到29万元（按年平均汇率折合4.4万美元）。</p>
+            </div>
+          
+        </a-modal>
     </div>
 </template>
 
@@ -59,6 +69,7 @@ export default {
         return {
             isRouterAlive:true,
             spinning:true,
+            introduction_visible: false,
         };
     },
     computed: {
